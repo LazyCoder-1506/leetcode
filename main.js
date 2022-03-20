aa8c7105bb9d79ff503b9e692d0c62c2650d9082
LazyCoder-1506 <pranayan1506@gmail.com>
2022-03-21 01:07:40 +0530

    loading screen, search

--- main.js
+++ main.js
@@ -9,6 +9,7 @@ function fetchQuestions () {
     format: "json"
   })
   .done(function (data) {
+    $('#questions_cont').html('')
     all_questions = data
     all_questions.sort((a,b) => a.likes < b.likes ? 1 : (a.likes > b.likes ? -1 : 0))
     var i = 0
@@ -53,3 +54,53 @@ function fetchQuestions () {
     $('[data-toggle="tooltip"]').tooltip()
   });
 }
+
+$('#search_box').keyup(function () {
+  var search_val = $('#search_box').val()
+  // $('#questions_cont').html(`<div class="mt-5 text-center">
+  //   <div class="spinner-border text_grey mb-2"></div>
+  //   <h4 class="font-weight-bold text_grey">Loading</h4>
+  // </div>`)
+  $('#questions_cont').html('')
+  var i = 0
+  for (let question_obj of all_questions) {
+    if (question_obj.title.toUpperCase().indexOf(search_val.toUpperCase()) > -1) {
+      i++
+      var question_url = 'https://leetcode.com/problems/' + question_obj.titleSlug
+      var tags_html = ''
+      for (let tag of question_obj.tagsArray) {
+        tags_html += `<span class="badge badge-pill mb-2 badge_tags ml-2">${tag}</span>`
+      }
+      var question_html = `<div class="row mx-0 pt-2 question_item ${i%2 ? 'alternate' : ''}" id="question_${question_obj.questionId}">
+        <div class="col-12 col-lg-6 mb-2">
+          <a href="${question_url}" class="question_link">${question_obj.questionId}. ${question_obj.title}</a>
+        </div>
+        <div class="col-4 col-md-2 col-lg-1 mb-2 text-center">
+          <span class="text-light" data-toggle="tooltip" data-placement="top" data-title="${question_obj.likes} likes">${question_obj.likes}</span>
+        </div>
+        <div class="col-4 col-md-2 col-lg-1 mb-2 text-center">
+          <span class="text-light" data-toggle="tooltip" data-placement="top" data-title="${question_obj.dislikes} dislikes">${question_obj.dislikes}</span>
+        </div>
+        <div class="col-4 col-md-2 col-lg-1 mb-2 text-center">
+          <span class="text-light" data-toggle="tooltip" data-placement="top" data-html="true" title="Accepted : ${question_obj.totalAccepted}<br>Submitted : ${question_obj.totalSubmission}<br>Acceptance Rate : ${question_obj.acRate}">${question_obj.acRate}</span>
+        </div>
+        <div class="col-4 col-md-2 col-lg-1 mb-2 text-center">
+          <span class="${question_obj.difficulty.toLowerCase()}">${question_obj.difficulty}</span>
+        </div>
+        <div class="col-4 col-md-2 col-lg-1 mb-2 text-center">
+          <span class="text-light">${question_obj.isPaidOnly ? 'Paid' : 'Free'}</span>
+        </div>
+        <div class="col-4 col-md-2 col-lg-1 mb-2 text-center">
+          ${question_obj.hasSolution ? '<i class="far fa-file-alt mx-1 solution_icon" data-toggle="tooltip" data-placement="top" data-title="Solution Available"></i>' : ''}
+          ${question_obj.hasVideoSolution ? '<i class="far fa-file-video mx-1 video_icon" data-toggle="tooltip" data-placement="top" data-title="Video Solution Available"></i>' : ''}
+        </div>
+        <div class="col-12 mb-2">
+          <span class="badge badge-pill mb-2 badge_category">${question_obj.categoryTitle}</span>
+          ${tags_html}
+        </div>
+      </div>`
+      $('#questions_cont').append(question_html)
+    }
+  }
+  $('[data-toggle="tooltip"]').tooltip()
+})
